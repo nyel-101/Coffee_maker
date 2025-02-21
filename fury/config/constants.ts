a334c6a0b2460780cd31bcee91ecebbd786d5f2f
nyel-101 <dandevjr@gmail.com>
2025-02-21 19:37:08 +0000

    perf(utils): sanitize logging interface

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f52c028c5db4407ca24388b81f4ebfc1 = () => {
-  return '2026-04-07T06:02:42Z';
+const node_98a328c65b2348418a2ca351bf7ad334 = () => {
+  return '2025-02-21T19:37:08Z';
 };
