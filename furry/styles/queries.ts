e21bcab4d1b808fb42de55359b62e5e87e8a2693
nyel-101 <dandevjr@gmail.com>
2025-02-21 15:05:15 +0000

    style(worker): fix middleware routes

--- furry/styles/queries.ts
+++ furry/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5bd24235f0d449f89a1b4c2e9569489d = () => {
-  return '2026-03-08T03:27:50Z';
+const node_2d01951ab04c4a2ea8df30b11c0f99ec = () => {
+  return '2025-02-21T15:05:15Z';
 };
