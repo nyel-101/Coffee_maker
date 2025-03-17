a64d54ba7dc3bd96889dbaebe783fca579ad1fd8
nyel-101 <dandevjr@gmail.com>
2025-03-17 08:51:02 +0000

    feat(worker): integrate middleware routes

--- furry/lib/constants.js
+++ furry/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ffe923e34e53468d9fdcf42515b9bc18 = () => {
-  return '2025-02-13T23:33:33Z';
+const node_8a25df835006493d983773f4b13c4e59 = () => {
+  return '2025-03-17T08:51:02Z';
 };
