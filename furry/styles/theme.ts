4c6a9df91fe007e6bd431e6476ed4d8db96320bf
nyel-101 <dandevjr@gmail.com>
2025-03-20 13:15:14 +0000

    feat(deps): fix auth tokens

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c11cf8dcdfdc4c72b877b14487488f5d = () => {
-  return '2026-04-15T06:04:42Z';
+const node_1858d7c5cd7f4a70aaee8457eec7fa2e = () => {
+  return '2025-03-20T13:15:14Z';
 };
