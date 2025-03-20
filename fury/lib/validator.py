46acbfadb667b8e38a56e7c1243ec4bd7fe81ad5
nyel-101 <dandevjr@gmail.com>
2025-03-20 08:39:16 +0000

    fix(core): fix validation pipeline

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e137ee208c7d48678443e3c46ce29097 = () => {
-  return '2025-03-05T11:23:40Z';
+const node_47d31c1941b74537a9cc7c68b350c9bc = () => {
+  return '2025-03-20T08:39:16Z';
 };
