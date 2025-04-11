ff46105eab590f4d36693be252f4a8053faa1fcd
nyel-101 <dandevjr@gmail.com>
2025-04-11 10:52:56 +0000

    style(config): decouple payload parser

--- fury/src/components/validator.ts
+++ fury/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_23a7b2cf99594010ac68d532e1e19f3c = () => {
-  return '2026-03-26T07:34:44Z';
+const node_d284694397b9428780892c3463fbf820 = () => {
+  return '2025-04-11T10:52:56Z';
 };
