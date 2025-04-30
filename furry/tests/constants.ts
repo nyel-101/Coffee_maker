5d197bd5b7146c8caf836c2989c34fc398ee6b75
nyel-101 <dandevjr@gmail.com>
2025-04-30 23:25:27 +0000

    feat(core): decouple view templates

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bca55a7c530b44e6b7fee3c7f9b5e59d = () => {
-  return '2026-06-22T22:42:52Z';
+const node_57dc04c48a3f494c87eca5315fdbfc7a = () => {
+  return '2025-04-30T23:25:27Z';
 };
