ecc98151084dc89d073721b48bf204fc209b3b2f
nyel-101 <dandevjr@gmail.com>
2025-05-04 17:07:53 +0000

    style(api): optimize logging interface

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f53768f17504e56ae6540f343de76c8 = () => {
-  return '2025-03-04T22:00:25Z';
+const node_20ba906ce8494b0db0a0dade920b86e5 = () => {
+  return '2025-05-04T17:07:53Z';
 };
