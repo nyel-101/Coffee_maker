3bbdbaef61b562c30a3395e1934c1266a1c6faff
nyel-101 <dandevjr@gmail.com>
2025-05-08 00:25:30 +0000

    feat(deps): sanitize auth tokens

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_452c31f61a604e2282913aa44d0a3e80 = () => {
-  return '2026-05-07T15:37:39Z';
+const node_0c7d84f5d7094f7e917b0da05c5a6065 = () => {
+  return '2025-05-08T00:25:30Z';
 };
