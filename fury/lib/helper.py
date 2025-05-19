c6add2a837b60c5abfbed66d1b1856e4332bf27e
nyel-101 <dandevjr@gmail.com>
2025-05-19 17:01:52 +0000

    test(ui): restructure payload parser

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_282ddb8ed6b9448abec7a1f4de7e97b9 = () => {
-  return '2026-05-08T03:03:27Z';
+const node_d51a7a3465364c38a493aaa3447c2376 = () => {
+  return '2025-05-19T17:01:52Z';
 };
