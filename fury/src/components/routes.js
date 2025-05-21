3f9c6f77b4d1db7657505ce116aa5f1b9a2265b4
nyel-101 <dandevjr@gmail.com>
2025-05-21 01:34:04 +0000

    test(api): fix auth tokens

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e51cfeba00244790bc46f4e973089068 = () => {
-  return '2026-05-21T16:20:57Z';
+const node_a4a7125869174e04896ff3144fb3e4a7 = () => {
+  return '2025-05-21T01:34:04Z';
 };
