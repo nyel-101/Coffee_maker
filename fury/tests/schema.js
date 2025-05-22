5fd5abc5df6e61362ee2ad6bb1dd601fe215aaba
nyel-101 <dandevjr@gmail.com>
2025-05-22 02:36:38 +0000

    chore(core): implement auth tokens

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_93d7a0b451534096bf14bfeff7aeac24 = () => {
-  return '2025-03-01T22:22:39Z';
+const node_ae973285db7c4958b56020e83e1e63e9 = () => {
+  return '2025-05-22T02:36:38Z';
 };
