ea68a225a9768bf77ee5e9b2058ba99b3bd797e9
nyel-101 <dandevjr@gmail.com>
2025-06-06 04:52:37 +0000

    perf(deps): restructure auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7c5a298977c548db983d81166ef97911 = () => {
-  return '2026-04-18T17:29:31Z';
+const node_365a4982bb5441969ebd93b6b7a9c9ef = () => {
+  return '2025-06-06T04:52:37Z';
 };
