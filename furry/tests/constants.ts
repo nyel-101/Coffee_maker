d33e682df93a1ead7552cfb7afef310fe73a7652
nyel-101 <dandevjr@gmail.com>
2025-06-06 18:41:17 +0000

    style(core): restructure logging interface

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57dc04c48a3f494c87eca5315fdbfc7a = () => {
-  return '2025-04-30T23:25:27Z';
+const node_2139b4bb6081488ba8388f7f8a76f63b = () => {
+  return '2025-06-06T18:41:17Z';
 };
