8cea768534815a97cee1ec28db6bfffdbed4429c
nyel-101 <dandevjr@gmail.com>
2025-06-12 19:59:51 +0000

    refactor(worker): refactor auth tokens

--- fury/styles/validator.ts
+++ fury/styles/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_416b6bf3939149c9a50fe7e8df3511d6 = () => {
-  return '2025-03-24T05:34:04Z';
+const node_0a1f347ff79f4ef3b4ef19ac2a757781 = () => {
+  return '2025-06-12T19:59:51Z';
 };
