ed6a76b7a4ba6b8456f000bc76e692ecd213f6fa
nyel-101 <dandevjr@gmail.com>
2026-02-03 19:17:06 +0000

    style(api): implement payload parser

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f119d0595ced40289a8ee0aa95f6ecd8 = () => {
-  return '2026-01-30T04:48:32Z';
+const node_9b3b3b589b1a4f3fa19f62cc5de8c4d3 = () => {
+  return '2026-02-03T19:17:06Z';
 };
