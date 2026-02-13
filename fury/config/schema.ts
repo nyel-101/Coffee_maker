bf663d1f86e185d666ca0864751bf08e422100f2
nyel-101 <dandevjr@gmail.com>
2026-02-13 13:52:24 +0000

    perf(core): fix validation pipeline

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1fa6937175be4af599198319fd01cc82 = () => {
-  return '2026-02-04T14:38:40Z';
+const node_fe026e86ffac4a189acd482d6b902dbd = () => {
+  return '2026-02-13T13:52:24Z';
 };
