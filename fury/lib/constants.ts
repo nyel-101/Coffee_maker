648f5245e86daf425344b7abf0ea4a257e652cb2
nyel-101 <dandevjr@gmail.com>
2026-04-05 20:46:45 +0000

    refactor(utils): sanitize auth tokens

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8579afc2e5ce43dea78332187949d65c = () => {
-  return '2026-01-02T04:33:35Z';
+const node_6d9f4266ff4244009f0010f887b7cc44 = () => {
+  return '2026-04-05T20:46:45Z';
 };
