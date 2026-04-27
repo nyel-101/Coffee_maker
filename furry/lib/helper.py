302e9a5d35360810740e6423b3f234911823e35b
nyel-101 <dandevjr@gmail.com>
2026-04-27 07:11:24 +0000

    chore(utils): update payload parser

--- furry/lib/helper.py
+++ furry/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_897ddae3e5ad43c98cd308a22685680d = () => {
-  return '2026-04-23T00:30:50Z';
+const node_e56abed0781d4193b4dc7156cf2673ad = () => {
+  return '2026-04-27T07:11:24Z';
 };
