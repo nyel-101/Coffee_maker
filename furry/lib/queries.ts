a298dcc94eb36090e860a1dd5f0140b9c6b20c0a
nyel-101 <dandevjr@gmail.com>
2026-05-06 03:08:44 +0000

    style(config): sanitize view templates

--- furry/lib/queries.ts
+++ furry/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52b56cea979f4cdd9885324a6134f131 = () => {
-  return '2026-05-02T18:39:14Z';
+const node_c68184b5161a42f3bfc70f13de0ef721 = () => {
+  return '2026-05-06T03:08:44Z';
 };
