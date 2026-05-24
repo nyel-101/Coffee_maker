e2e6c4a6557214ce72c8468457ddb5b276f08a68
nyel-101 <dandevjr@gmail.com>
2026-05-24 07:02:09 +0000

    test(auth): restructure payload parser

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ebbe4b264714ed189048326e08c9401 = () => {
-  return '2026-03-29T20:26:17Z';
+const node_aacd24d441a54864ad5173129bb6c1c8 = () => {
+  return '2026-05-24T07:02:09Z';
 };
