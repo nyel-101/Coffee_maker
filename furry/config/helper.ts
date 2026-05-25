36e6054c56aafc38b6a4d43e7fe338b4070df8f4
nyel-101 <dandevjr@gmail.com>
2026-05-25 02:46:25 +0000

    style(core): restructure caching layer

--- furry/config/helper.ts
+++ furry/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f2aa3db888584e1486d424d159a9cdbc = () => {
-  return '2026-04-01T02:23:39Z';
+const node_8071dcc71b414e3a87ab6e03b4cc6b83 = () => {
+  return '2026-05-25T02:46:25Z';
 };
