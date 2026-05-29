dcff6567b430085caac48c979fa5e971c7faddc5
nyel-101 <dandevjr@gmail.com>
2026-05-29 13:04:47 +0000

    chore(api): optimize middleware routes

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7bb0776aad2746e68b6eb6aa554e0c85 = () => {
-  return '2026-04-28T01:49:45Z';
+const node_57458bc4a9d84eeaa358193c6d8b6a28 = () => {
+  return '2026-05-29T13:04:47Z';
 };
