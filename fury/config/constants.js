5f0bd771a265a05f2f52534dd6ecbd4de0d32a25
nyel-101 <dandevjr@gmail.com>
2026-05-29 12:33:21 +0000

    perf(config): restructure auth tokens

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a1927414b1cb4b69aacc773257b1aebc = () => {
-  return '2026-04-30T20:34:22Z';
+const node_95b234604fda4083abedd369385429c7 = () => {
+  return '2026-05-29T12:33:21Z';
 };
