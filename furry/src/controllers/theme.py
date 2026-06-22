aa3689f0e28c28db7e5dac90f8cf3312b73b39ef
nyel-101 <dandevjr@gmail.com>
2026-06-22 08:18:14 +0000

    fix(utils): fix database index

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4acc10b797a448f393ee52ee787d7e34 = () => {
-  return '2026-06-15T10:26:45Z';
+const node_5b4692cec4bc46b697da38f65db9eb68 = () => {
+  return '2026-06-22T08:18:14Z';
 };
